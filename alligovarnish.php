5929ddc58b9730ad859edd3224413758d1e00a7d
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-12-20 06:42:04 -0200

    Criado _getTimeAsSeconds(), setCacheBrowser() e setCacheProxy()

--- alligovarnish.php
+++ alligovarnish.php
@@ -66,6 +66,46 @@ class plgSystemAlligovarnish extends JPlugin
     protected $debug = [];
 
     /**
+     * Debug is enabled?
+     *
+     * @var Boolean
+     */
+    protected $debug_is = false;
+
+    private function _getTimeAsSeconds($time)
+    {
+        $seconds = 0;
+        if (!empty($time)) {
+            switch (substr($time, -1)) {
+                case 's':
+                    $seconds = (int) substr($time, 0, -1);
+                    break;
+                case 'm':
+                    $seconds = (int) substr($time, 0, -1);
+                    $seconds = $seconds * 60;
+                    break;
+                case 'd':
+                    $seconds = (int) substr($time, 0, -1);
+                    $seconds = $seconds * 60 * 24;
+                    break;
+                case 'm':
+                    $seconds = (int) substr($time, 0, -1);
+                    $seconds = $seconds * 60 * 24 * 30;
+                    break;
+                case 'y':
+                    $seconds = (int) substr($time, 0, -1);
+                    $seconds = $seconds * 60 * 24 * 30 * 365;
+                    break;
+                default:
+                    $seconds = (int) $time;
+                    break;
+            }
+        }
+
+        return $seconds;
+    }
+
+    /**
      * Explode lines and itens separed by : and return and array,
      * with debug option if syntax error
      * 
@@ -90,7 +130,7 @@ class plgSystemAlligovarnish extends JPlugin
                     } else if (!in_array(substr($itemid_hour[1], -1), ['s', 'h', 'd', 'w', 'm', 'y'])) {
                         $this->debug['wrongtime'] = empty($this->debug['wrongtime']) ? $line : $this->debug['wrongtime'] . ',' . $line;
                     } else {
-                        $times[(int) $itemid_hour[0]] = $itemid_hour[1];
+                        $times[(int) $itemid_hour[0]] = $this->_getTimeAsSeconds($itemid_hour[1]);
                     }
                 }
             }
@@ -100,33 +140,87 @@ class plgSystemAlligovarnish extends JPlugin
     }
 
     /**
+     * Set headers specific for the browser cache
+     * 
+     * @param   Integer   $time
+     */
+    public function setCacheBrowser($time = null)
+    {
+        if (empty($time)) {
+            JFactory::getApplication()->allowCache(false);
+            JFactory::getApplication()->setHeader('Cache-Control', 'no-cache, no-store, must-revalidate', true);
+            JFactory::getApplication()->setHeader('Pragma', 'no-cache', true);
+            JFactory::getApplication()->setHeader('Expires', '0', true);
+            if ($this->debug_is) {
+                JFactory::getApplication()->setHeader('X-Alligo-BrowserCache', 'disabled');
+            }
+        } else {
+            $epoch = strtotime('+' . $time . 's');
+
+            JFactory::getApplication()->allowCache(true);
+            JFactory::getApplication()->setHeader('Cache-Control', 'public, max-age=' . $time, true);
+            JFactory::getApplication()->setHeader('Pragma', 'cache', true);
+            JFactory::getApplication()->setHeader('Expires', date('D, j M Y H:i:s T', $epoch), true);
+            if ($this->debug_is) {
+                JFactory::getApplication()->setHeader('X-Alligo-BrowserCache', 'enabled, ' . $time . 's, datetime ' . date('D, j M Y H:i:s T', $epoch));
+            }
+        }
+    }
+
+    /**
+     * Set headers specific for the proxy cache
+     * 
+     * @param   Integer   $time
+     */
+    public function setCacheProxy($time = null)
+    {
+        if (empty($time)) {
+            JFactory::getApplication()->setHeader('Surrogate-Control', 'no-store', true);
+            if ($this->debug_is) {
+                JFactory::getApplication()->setHeader('X-Alligo-ProxyCache', 'disabled');
+            }
+        } else {
+            $epoch = strtotime('+' . $time . 's');
+            JFactory::getApplication()->setHeader('Surrogate-Control', 'public, max-age=' . $time, true);
+            if ($this->debug_is) {
+                JFactory::getApplication()->setHeader('X-Alligo-ProxyCache', 'enabled, ' . $time . 's, datetime ' . date('D, j M Y H:i:s T', $epoch));
+            }
+        }
+    }
+
+    /**
      * onAfterInitialise
      * 
-     * @package  ImNotJoomla
-     * @since    1.6
-     * 
-     * @deprecated
+     * This event is triggered after the framework has loaded and the application initialise method has been called.
      * 
      * @return   void
      */
     public function onAfterInitialise()
     {
         $this->is_site = JFactory::getApplication()->isSite();
-//        if (JFactory::getApplication()->isSite()) {
-//            $this->_cleanHeaders();
-//        }
     }
 
+    /**
+     * This event is triggered before the framework creates the Head section of the Document.
+     */
     public function onBeforeCompileHead()
     {
-        if (JFactory::getApplication()->isSite()) {
-            $this->_cleanHeaders();
-        }
+
+        // @todo Feature: maybe implement a way to set robots "nofollow" if site
+        // is not behind varnish cache
         //if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] !== 80) {
         //    JFactory::getDocument()->setMetaData('robots', 'noindex, nofollow');
         //}
     }
 
+    /**
+     * This event is triggered immediately before the framework has rendered
+     *  the application. 
+     * 
+     * Rendering is the process of pushing the document buffers into the 
+     * template placeholders, retrieving data from the document and pushing it 
+     * into the JResponse buffer.
+     */
     public function onBeforeRender()
     {
         if ($this->is_site) {
@@ -134,6 +228,13 @@ class plgSystemAlligovarnish extends JPlugin
             $this->itemid = JFactory::getApplication()->getMenu()->getActive()->id;
             $this->exptproxy = $this->_getTimes($this->params->get('exptproxy', ''));
             $this->exptbrowser = $this->_getTimes($this->params->get('exptbrowser', ''));
+            $this->debug_is = (bool) $this->params->get('debug', false);
+            $this->setCacheBrowser(300);
+            $this->setCacheProxy(300);
+        } else {
+
+            // Tip for varnish that we REALLY do not want cache this
+            $this->setCacheProxy(false);
         }
 
 //        if (JFactory::getApplication()->isSite()) {
@@ -142,16 +243,22 @@ class plgSystemAlligovarnish extends JPlugin
     }
 
     /**
-     * onAfterRender
+     * This event is triggered after the framework has rendered the application.
+     * 
+     * Rendering is the process of pushing the document buffers into the 
+     * template placeholders, retrieving data from the document and pushing 
+     * it into the JResponse buffer.
+     * 
+     * When this event is triggered the output of the application is available in the response buffer.
      * 
      * @return   void
      */
     public function onAfterRender()
     {
-        if (JFactory::getApplication()->isSite()) {
-            $this->_cleanHeaders();
-            session_unset();
-        }
+//        if (JFactory::getApplication()->isSite()) {
+////            $this->_cleanHeaders();
+////            session_unset();
+//        }
 //        if (JFactory::getApplication()->isSite()) {
 //
 //            // Destroi a sessão apenas se for usuário do site
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# Plugin Alligo Varnish
+# Plugin Alligo Varnish - 1.0RC
 Joomla CMS plugin that allow the CMS make a more agressive cache with Varnish Cache 4
 
